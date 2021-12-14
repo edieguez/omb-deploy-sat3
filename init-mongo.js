50c1f4a9b5525789b9eee1d3c494e9562d9f3ca3
Forgived <yulat.lira.12@gmail.com>
2021-12-13 18:36:56 -0600

    Change default account structure

--- init-mongo.js
+++ init-mongo.js
@@ -4,17 +4,21 @@ db.createCollection("accounts", { capped: false });
 
 db.accounts.insert([
   {
-    name: "Account 1",
+    name: "Jaimito Cartero",
     routingNumber: "1234",
     accountNumber: "9876",
     openDate: "2021-03-28",
     currentBalance: 125,
+	dateOfBirth: "1992-10-09",
+	accountType: "CHECKING",
   },
   {
-    name: "Account 2",
+    name: "Chente Fernandez",
     routingNumber: "1234",
     accountNumber: "9877",
     openDate: "2020-01-17",
     currentBalance: 0,
+	dateOfBirth: "1995-07-19",
+	accountType: "SAVINGS",
   },
 ]);
